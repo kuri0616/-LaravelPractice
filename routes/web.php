f0ee8f69ab0a1d8cee766d4430c554153a196f32
日下陸矢 <rikuya.98@gmail.com>
2024-11-27 21:11:26 +0900

    init2

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactFormController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tests/test', [TestController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('contact')->middleware('auth')
+    ->Controller(ContactFormController::class)
+    ->name('contact.')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
+
+
+require __DIR__.'/auth.php';
